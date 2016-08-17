d8f2e70cebb5064998bba6183caa7ffd776c8152
irplagura <irplagura@users.noreply.github.com>
2016-08-17 13:01:06 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,6 +3,13 @@ class nginx  {
     ensure => present,
   }
   
+  file {'/var/www/':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  
   file {'/var/www/index.html':
     ensure => file,
     owner => 'root',
