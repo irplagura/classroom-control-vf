6fd7b80ecc60473843717de592dcf7b3d5146f78
irplagura <irplagura@users.noreply.github.com>
2016-08-18 08:05:14 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,40 @@
 class nginx  {
   File {
+    ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0664',
   }
   
+  $docroot = '/var/www'
+  $confdir = '/etc/nginix'
+  
   package {'nginx':
     ensure => present,
   }
   
-  file {'/var/www/':
+  file { $docroot :
     ensure => directory,
     }
   
-  file {'/var/www/index.html':
-    ensure => file,
+  file '${docroot}/index.html':
     source => 'puppet:///modules/nginx/index.html',
   }
   
-  file {'/etc/nginx/nginx.conf':
-    ensure => file,
+  file {'${confdir}nginx.conf':
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   
-  file {'/etc/nginx/conf.d':
+  file {'${confdir}/conf.d':
     ensure => directory,
     source  => 'puppet:///modules/nginx/nginx.conf',    
     require => Package['nginx'],    
     notify  => Service['nginx'], 
     }
     
-  file {'/etc/nginx/conf.d/default.conf':
-    ensure => file,
+  file {'${confdir}/conf.d/default.conf':
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
